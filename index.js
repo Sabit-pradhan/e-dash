c3000f5507efbba4b4ca128239c27b78dfaa8c80
Sabit <sabitpradhan4@gmai.com>
2024-01-07 22:52:51 +0530

    api updated

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-const express = require('express');
-
+const express = require("express");
+const cors = require("cors");
+require('./db/config');
+const User = require('./db/User');
+const Product = require("./db/Product");
 const app = express();
 
-app.get("/",(req,resp)=>{
-    resp.send("app is working")
-});
+app.use(express.json());
+app.use(cors());
 
-app.listen(5000)
+app.post("/register", async(req,resp)=>{
+    let newUser= new User(req.body);
+    let result = await newUser.save();
+    result =  result.toObject();
+    delete result.password
+    resp.send(result);
+})
+app.post("/login",async(req,resp)=>  {
+    console.log(req.body)
+    if(req.body.password && req.body.email){
+    let user=await User.findOne(req.body).select("-password");
+    if(user){
+        resp.send(user)
+
+    }else{
+        resp.send({result:'no User Found'})
+    }
+}else{
+    resp.send({result:'no User Found'})
+}
+
+})
+app.post("/add-product",async(req, resp)=>{
+let product =new Product(req.body);
+let result = await product.save();
+resp.send(result)
+})
+
+app.listen(5000); 
